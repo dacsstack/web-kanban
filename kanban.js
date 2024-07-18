910c5ff5a515730aabfa83b3acf0daed6102cb9d
dacsstack <157571986+dacsstack@users.noreply.github.com>
2024-07-17 21:17:48 -0800

    Update kanban.js

--- kanban.js
+++ kanban.js
@@ -1,9 +1,10 @@
 export default class Kanban {
-    //Get Task
-    static getTasks(columnId) {
+
+    static getTasks(columnId){
         const data = read().find(column => {
             return column.columnId == columnId;
         });
+        
         return data.tasks;
     }
 
@@ -25,12 +26,8 @@ export default class Kanban {
         return task;
     }
 
-    //UPDATE COLUMN AND DATA 
     static updateTask(taskId, updatedInformation) {
-        //Reference the data
         const data = read();
-
-        //Get Current column or Information
         function findColumnTask() {
             for (const column of data) {
 
@@ -44,81 +41,41 @@ export default class Kanban {
                     return [task, column];
                 }
             }
-            //Turn it to function intead
-            /* for (const column of data) {
-                //find task
-                const task = column.tasks.find(item => {
-                    return item.taskId == taskId;
-                });
-                if (task) {
-                    console.log(column, task);
-                }
-     */
-            //=======End of Current Column================
-        }
-        //print element
-        //console.log(findColumnTask());
-
-        //Print endividual one task OR Reference the task
         const [task, currentColumn] = findColumnTask();
-        //console.log(task); //print sample
-
-        //Find new targeted column
         const targetColumn = data.find(column => {
             return column.columnId == updatedInformation.columnId;
         });
-
-        //update task content
         task.content = updatedInformation.content;
-
-        //detelete current column
         currentColumn.tasks.splice(currentColumn.tasks.indexOf(task), 1);
-
-        //if find the targeted column Push/ Add
         targetColumn.tasks.push(task);
-
-        //save the data
         save(data);
     }
-
-    //Delete
     static deleteTask(taskId) {
         const data = read();
 
-        for (const column of data) {//find all column
-            const task = column.tasks.find(item => { //find all task item
-                return item.taskId == taskId; // return
+        for (const column of data) {
+            const task = column.tasks.find(item => { 
+                return item.taskId == taskId;
             });
-
-            //put if condition if you want delete only a specific task
             if (task) {
                 column.tasks.splice(column.tasks.indexOf(task), 1);
             }
-
-            //Remove Elements from an array 
-            //column.tasks.splice(column.tasks.indexOf(task), 1);
         }
-        //save to local storage
         save(data);
     }
-
     static getAllTasks() {
         //Call Read function
         const data = read();
         //Add ColumnCount
         columnCount();
         //return data; //Object format
-        return [data[0].tasks, data[1].tasks, data[2].tasks]; // Array Proper format
+        return [data[0].tasks, data[1].tasks, data[2].tasks];
 
     }
 }
 
 function read() {
-    //first Capture the data on local storage
     const data = localStorage.getItem("data");
-
-    //If empty on local storage
-    //JavaScript Format no need to JSON.parse 
     if (!data) {
         return [
             { colomnId: 0, tasks: [] },
@@ -126,18 +83,14 @@ function read() {
             { colomnId: 2, tasks: [] }
         ];
     }
-    //return it
     return JSON.parse(data);
 
 }
 
 function save(data) {
     localStorage.setItem("data", JSON.stringify(data));
-
-    //Add Column Count function
     columnCount();
 }
-
 function columnCount() {
     const data = read();
 
